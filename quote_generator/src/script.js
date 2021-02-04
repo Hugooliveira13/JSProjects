9872b764d6c653434d9d63642c1d9c18b3130a59
Hugo <Hugomso13@gmail.com>
2021-02-04 17:23:34 +0000

    update

--- quote_generator/src/script.js
+++ quote_generator/src/script.js
@@ -1,3 +1,10 @@
+const quoteContainer = document.getElementById(quote_container);
+const quoteText = document.getElementById(quote);
+const authorText = document.getElementById(quote_author);
+const twitter_btn = document.getElementById(twitter_btn);
+const newQuoteBtn = document.getElementById(new_quote);
+
+
 // Get quote from API
 async function getQuote(){
     const proxyUrl = `https://cors-anywhere.herokuapp.com/`
@@ -6,7 +13,7 @@ async function getQuote(){
     try{
         const response = await fetch(proxyUrl+apiUrl);
         const data = await response.json();
-        console.log(data);
+        
     }catch(error){
         getQuote();
         console.log("Upsy Daisy. Try again", error);
